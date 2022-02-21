2a4442a169de3843eb960e5eebefa79c65d13ffc
songdev <songdev0310@gmail.com>
2022-02-22 01:46:55 +0900

    develope initial commit

--- src/FirstTest01.java
+++ src/FirstTest01.java
@@ -1,5 +1,5 @@
 public class FirstTest01 {
     public static void main(String[] args) {
-        System.out.println(1);
+        System.out.println(1111111);
     }
 }
---
+++ src/FirstTest02.java
@@ -0,0 +1,5 @@
+public class FirstTest02 {
+    public static void main(String[] args) {
+        System.out.println(222222);
+    }
+}
