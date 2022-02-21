7072a1d04fcc27737f50d9ead01c31651e1f968c
songdev <songdev0310@gmail.com>
2022-02-22 01:58:34 +0900

    develop 4번째

--- src/FirstTest04.java
+++ src/FirstTest04.java
@@ -1,5 +1,6 @@
 public class FirstTest04 {
     public static void main(String[] args) {
         System.out.println("develope 에서 04번 클래스 생성");
+        System.out.println("develope 에서 issue_branch 병합후 작성");
     }
 }
