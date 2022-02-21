50fb3fa86faeb36b743df583f9eb2eba322280fa
songdev <songdev0310@gmail.com>
2022-02-22 01:53:11 +0900

    issue_112_test_002

--- src/FirstTest03.java
+++ src/FirstTest03.java
@@ -1,5 +1,6 @@
 public class FirstTest03 {
     public static void main(String[] args) {
         System.out.println("issue_112_test_001");
+        System.out.println("issue_112_test_002");
     }
 }
